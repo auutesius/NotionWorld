25965b7295ecd319c2596d0531bc3635c1f0d28a
李钦 <447945947@qq.com>
2020-08-11 20:07:44 +0800

    Remove Scene
    移除了无用的场景。

--- Assets/Scripts/SpecialEffects/ClickEmit.cs
+++ Assets/Scripts/SpecialEffects/ClickEmit.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickEmit : MonoBehaviour {
+public class ClickEmit : MonoBehaviour
+{
     public RippleEffect ripple;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("press mouse .");
-            ripple.Emit(new Vector2(0.5f,1f));
+            //Debug.Log("press mouse .");
+            ripple.Emit(new Vector2(0.5f, 1f));
         }
-	}
+    }
 }
