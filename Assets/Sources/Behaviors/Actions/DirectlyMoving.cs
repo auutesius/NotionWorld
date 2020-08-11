65c895490bdef3d662597340b8f361abcd44dfc8
李钦 <447945947@qq.com>
2020-08-11 20:34:45 +0800

    Remove PlayerMovement
    移除了过时的组件。

---
+++ Assets/Sources/Behaviors/Actions/DirectlyMoving.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using NotionWorld.Entities;
+using UnityEngine;
+
+namespace NotionWorld.Behaviors
+{
+    [TaskCategory("NotionWorld")]
+    public sealed class DirectlyMoving : Action
+    {
+        [BehaviorDesigner.Runtime.Tasks.Tooltip("The GameObject that tracks.")]
+        public SharedGameObject targetGameObject;
+
+        private Vector3 direction;
+
+        private EntityMovement movement;
+
+        [BehaviorDesigner.Runtime.Tasks.Tooltip("Move Trend of GameObject.")]
+        public MoveTrends trend;
+
+        public enum MoveTrends
+        {
+            Towards, Away
+        }
+
+        public override void OnAwake()
+        {
+            movement = Owner.GetComponent<EntityMovement>();
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            movement.Direction = MoveDirection(trend);
+            return TaskStatus.Success;
+        }
+
+        private Vector2 MoveDirection(MoveTrends trend)
+        {
+            Vector2 direction;
+            switch (trend)
+            {
+                case MoveTrends.Away:
+                    direction = transform.position - targetGameObject.Value.transform.position;
+                    break;
+                default:
+                    direction = targetGameObject.Value.transform.position - transform.position;
+                    break;
+            }
+            direction = direction.normalized;
+            return direction;
+        }
+    }
+
+}
