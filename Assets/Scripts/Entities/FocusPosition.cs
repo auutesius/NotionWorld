8989dc39f964d927ffef7f1b1dfdafaba287e92f
李钦 <447945947@qq.com>
2020-08-17 17:26:33 +0800

    Update AI
    离玩家过远时不会开火。

---
+++ Assets/Scripts/Entities/FocusPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Vector2 position;
+
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = position;
+    }
+}
