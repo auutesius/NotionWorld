afc82e26cefa0b7f4a735c08e2f77a13177f4fc5
李钦 <447945947@qq.com>
2020-08-17 20:52:03 +0800

    Update Player

--- Assets/Scripts/Entities/PlayerLive.cs
+++ Assets/Scripts/Entities/PlayerLive.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 using NotionWorld.Capabilities;
 using NotionWorld.Entities;
 using NotionWorld.Worlds;
+using UnityEngine.SceneManagement;
 
 public class PlayerLive : MonoBehaviour
 {
     private Health health;
 
-    private float endingTime = 2F;
+    public float endingTime = 2F;
 
-    private float endingScale = 0.5F;
+    public float endingScale = 0.5F;
+
+    public float sceneTime = 4F;
+
+    public Animator animator;
+
+    public UIController uIController;
 
     void Start()
     {
@@ -23,15 +30,25 @@ public class PlayerLive : MonoBehaviour
         if (health.Value < 0)
         {
             health.Value = health.MaxValue;
-            
+
             StartCoroutine(EndCorotinue());
         }
     }
 
     private IEnumerator EndCorotinue()
     {
+        uIController.enabled = false;
+        animator.SetTrigger("Die");
+
         Time.timeScale = endingScale;
         yield return new WaitForSeconds(endingTime);
         Time.timeScale = 1;
+
+        var fadeCanvas = ObjectPool.GetObject("FadeCanvas", "UI");
+        var fade = fadeCanvas.GetComponent<Fade>();
+        fade.FadeOut();
+
+        yield return new WaitForSeconds(sceneTime);
+        
     }
 }
---
+++ Assets/Scripts/UI/Fade.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    
+    public float fadeTime;
+
+    private float alpha = 0;
+
+    private float targetAlpha = 1;
+
+    public Image fadeImage;
+
+    public void FadeIn()
+    {
+        alpha = 1;
+        targetAlpha = 0;
+        StartCoroutine(FadeCorotinue());
+    }
+
+    public void FadeOut()
+    {
+        alpha = 0;
+        targetAlpha = 1;
+        StartCoroutine(FadeCorotinue());
+    }
+
+    public IEnumerator FadeCorotinue()
+    {       
+        WaitForFixedUpdate wait = new WaitForFixedUpdate();
+        float delta = (targetAlpha - alpha) * Time.fixedDeltaTime / fadeTime;
+        float timer = fadeTime;
+        Color color = fadeImage.color;
+        color.a = alpha;
+        fadeImage.color = color;
+        while (timer > 0)
+        {
+            timer -= Time.fixedDeltaTime;
+            color.a += delta;
+            fadeImage.color = color;
+            Debug.Log(color.a);
+            yield return wait;
+        }
+    }
+}
