2b8c03bce6b33196e5a3770bfc7e332a8f6f271e
李钦 <447945947@qq.com>
2020-08-18 11:05:58 +0800

    Update exit

---
+++ Assets/Scripts/Entities/Exit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public sealed class Exit : MonoBehaviour
+{
+    public GameObject player;
+
+    public string nextScene;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
